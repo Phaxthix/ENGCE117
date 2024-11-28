2a67355c42de44f51e9f5f3d508b36f4e27f626b
Phanthip <phanthiphyphuththwngs@gmail.com>
2024-11-28 11:24:16 +0700

    updete

---
+++ Lab/Lab1.1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n ;
+    
+    printf( " Number : " ) ;
+    scanf( "%d", &n ) ;
+
+    for (int i = 0; i < n; i++)
+    {
+        printf( " Hello world\n " ) ;
+    }
+    
+ return 0 ;
+}
+
+
