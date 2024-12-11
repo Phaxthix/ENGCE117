7559074c76147ca5d0f9331c611d704c2e5d17e3
Phanthip <phanthiphyphuththwngs@gmail.com>
2024-12-12 01:49:49 +0700

    lap

--- Lab/Lab1.2.c
+++ Lab/Lab1.2.c
@@ -1,10 +1,40 @@
+/*เขียนคำสั่งในการสร้างและเรียกใช้งานฟังก์ชัน GetSet() เพื่อรับจำนวนสมาชิกและค่าของแต่ละสมาชิกในเซต จากคีย์บอร์ด ตามโปรโตไทป์ต่อไปนี้
+
+#include <stdio.h>
+
+int GetSet( int [] ) ;
+
+int main() {
+    int *data, num ;
+    num = GetSet( &data ) ;
+    return 0 ;
+}//end function
+*/
+
 #include <stdio.h>
 #include <string.h>
 
-void GetSet( int [], int * ) ;
+
+void GetSet( int a[], int *num ) ;
 
 int main() {
- int *data, num ;
- GetSet( &data, &num ) ;
- return 0 ;
+    int data[ 100 ] , n = 0 ;
+    GetSet( data, &n ) ;
+    return 0 ;
 }//end function
+
+
+void GetSet( int a[] , int *num ) {
+    printf( "Enter n : ") ;
+    scanf( "%d" , num ) ;
+
+    printf( "Enter data : " ) ;
+    for ( int i = 0 ; i < *num ; i++ ) {
+        scanf( "%d" , &a[i] ) ;
+    }//end for
+    
+    for ( int i = 0 ; i < *num ; i++ ) {
+        printf( "%d " , a[ i ] ) ;
+    }//end for
+    
+} //end GetSet
